09f5b02aed37fb00a7a3b45c643fab6ca7f42df0
SupremeMaster23 <gabydiaz2000@hotmail.com>
2023-03-08 14:19:20 -0600

    Conexion con elephansql, configuracion del idex.js y primer endpoint

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hi")
+const express = require("express");
+const PORT= 8080;
+const app=express();
+require('dotenv').config();
+const connectionString = process.env.CONNECTION_STRING;
+const pgp = require("pg-promise")()
+const db = pgp(connectionString)
+app.get("/tasks",async(req, res)=>{
+    try{
+        const result = await db.query('SELECT * FROM task_list');
+        res.json(result);
+        console.log("yay");
+    }catch (err) {
+        console.log(err);
+    }
+});
+
+app.listen(PORT, () => {console.log("yay")});
